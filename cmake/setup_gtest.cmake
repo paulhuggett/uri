0aa17cb8165f7015ff252abfb2f2d82804cfc0c6
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-11-30 20:57:47 +0100

    Use generator expressions rather than if(). (#26)
    Use target_compile_features rather than setting the CXX_STANDARD
    property.

--- cmake/setup_gtest.cmake
+++ cmake/setup_gtest.cmake
@@ -25,25 +25,15 @@ function (setup_gtest)
 
     set (gclangopts )
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang$")
-      check_cxx_compiler_flag (
-        -Wno-implicit-int-float-conversion
-        CLANG_W_NO_IMPLICIT_INT_FLOAT_CONVERSION
-      )
-      if (${CLANG_W_NO_IMPLICIT_INT_FLOAT_CONVERSION})
-        list (APPEND gclangopts -Wno-implicit-int-float-conversion)
-      endif ()
+      check_cxx_compiler_flag (-Wno-implicit-int-float-conversion CLANG_W_NO_IMPLICIT_INT_FLOAT_CONVERSION)
+      list (APPEND gclangopts $<$<BOOL:${CLANG_W_NO_IMPLICIT_INT_FLOAT_CONVERSION}>:-Wno-implicit-int-float-conversion>)
     endif ()
 
     set (is_clang $<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>)
 
     # Adjust compiler options for the gtest/gmock.
     foreach (target gtest gmock gmock_main gtest_main)
-      set_target_properties (
-        ${target}
-        PROPERTIES CXX_STANDARD ${STANDARD}
-                   CXX_STANDARD_REQUIRED Yes
-                   CXX_EXTENSIONS No
-      )
+      target_compile_features (${target} PUBLIC $<IF:$<BOOL:${URI_CXX17}>,cxx_std_17,cxx_std_20>)
       target_compile_definitions (
         ${target} PUBLIC GTEST_REMOVE_LEGACY_TEST_CASEAPI_=1
       )
